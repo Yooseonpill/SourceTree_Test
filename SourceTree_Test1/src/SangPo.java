70a5bfa66b00cc807cb200ec9b08f12898f8236a
Yooseonpill <nokysp@naver.com>
2022-05-25 16:05:29 +0900

    상속 --> 포함 코드변경

--- SourceTree_Test1/src/SangPo.java
+++ SourceTree_Test1/src/SangPo.java
@@ -2,19 +2,24 @@ class MyPoint {
 	int x,y;
 }
 
-class Circle extends MyPoint {
+//class Circle extends MyPoint {
+//	int r;
+//}
+
+class Circle {
+	MyPoint p = new MyPoint();
 	int r;
 }
 public class SangPo {
 
 	public static void main(String[] args) {
 		Circle c = new Circle();
-		c.x = 1;
-		c.y = 4;
+		c.p.x = 1;
+		c.p.y = 4;
 		c.r = 9;
 		
-		System.out.printf("c.x = %d%n", c.x);
-		System.out.printf("c.y = %d%n", c.y);
+		System.out.printf("c.p.x = %d%n", c.p.x);
+		System.out.printf("c.p.y = %d%n", c.p.y);
 		System.out.printf("c.r = %d%n", c.r);
 	}
 
