5fbab4795f35d75d053c44af0e0968a5d99fb561
Yooseonpill <nokysp@naver.com>
2022-05-25 16:21:35 +0900

    Object 클래스 역할 toString 활용

--- SourceTree_Test1/src/SangPo.java
+++ SourceTree_Test1/src/SangPo.java
@@ -6,7 +6,7 @@ class MyPoint {
 //	int r;
 //}
 
-class Circle {
+class Circle { // 부모가 없는 모든 클래스는 Object가 부모가 된다.
 	MyPoint p = new MyPoint();
 	int r;
 }
@@ -21,6 +21,8 @@ public class SangPo {
 		System.out.printf("c.p.x = %d%n", c.p.x);
 		System.out.printf("c.p.y = %d%n", c.p.y);
 		System.out.printf("c.r = %d%n", c.r);
+		System.out.println(c.toString()); //Object클래스 안에 toString메소드가 있어서 사용가능(주소값 반환)
+		System.out.println(c); // c가 참조변수라서 주소값을 반환
 	}
 
 }
