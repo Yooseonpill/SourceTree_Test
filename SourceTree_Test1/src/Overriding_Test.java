352f0f5d2fbf3fe1d913b19b3e8f2fba657482ab
Yooseonpill <nokysp@naver.com>
2022-05-25 16:52:14 +0900

    코드 간결화

--- SourceTree_Test1/src/Overriding_Test.java
+++ SourceTree_Test1/src/Overriding_Test.java
@@ -9,8 +9,14 @@ class Point1 {
 class Point2 extends Point1 {
 	int z;
 	
+	Point2(int x, int y, int z){ //생성자 초기화
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
 	// z를 return에 포함시키기 위해 오버라이딩을 해야함.
-	String text() { // z 오버라이딩 작업
+	public String toString() { //toString 메서드로 오버라이딩
 		return "x : "+x+", y : "+y+", z : " +z;
 	}
 }
@@ -18,11 +24,12 @@ class Point2 extends Point1 {
 public class Overriding_Test {
 
 	public static void main(String[] args) {
-		Point2 p = new Point2();
-		p.x = 2;
-		p.y = 4;
-		p.z = 8;
-		System.out.println(p.text());
+		Point2 p = new Point2(2,3,4);
+		System.out.println(p);
+//		p.x = 2;
+//		p.y = 4;
+//		p.z = 8;
+//		System.out.println(p.text());
 	}
 
 }
